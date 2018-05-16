4625f0c91d33486f3422fda1de3db5036c279692
NProof <34260623+NProof@users.noreply.github.com>
2018-05-17 00:01:14 +0800

    main algorithm

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc , char **argv)
+{
+	std::cout << "Take a dark chess's game." << std::endl;
+	std::cout << "Join first player and second player." << std::endl;
+	std::cout << "First player move in the game." << std::endl;
+	std::cout << "Do" << std::endl;
+	std::cout << "    Second player move in the game." << std::endl;
+	std::cout << "    If ( game is over )" << std::endl;
+	std::cout << "        then break." << std::endl;
+	std::cout << "    First player move in the game." << std::endl;
+	std::cout << "While ( game is not over )" << std::endl;
+	std::cout << "If ( game's trun is first )" << std::endl;
+	std::cout << "    then second player is winner." << std::endl;
+	std::cout << "Else" << std::endl;
+	std::cout << "    first player is winner." << std::endl;
+	return 0;
+} // end main(int argc , char **argv)
+
