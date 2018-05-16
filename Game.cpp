282e3035250e594f7e92ba21bb2aa565e54ea339
NProof <34260623+NProof@users.noreply.github.com>
2018-05-17 00:16:26 +0800

    update

---
+++ Game.cpp
@@ -0,0 +1,11 @@
+#include "Game.h"
+
+Game::Game()
+{
+	std::cout << "Take a dark chess's game." << std::endl;
+}
+
+Game::~Game()
+{
+	
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include "Game.h"
 
 int main(int argc , char **argv)
 {
-	std::cout << "Take a dark chess's game." << std::endl;
+	new Game(); // std::cout << "Take a dark chess's game." << std::endl;
 	std::cout << "Join first player and second player." << std::endl;
 	std::cout << "First player move in the game." << std::endl;
 	std::cout << "Do" << std::endl;
@@ -15,6 +16,19 @@ int main(int argc , char **argv)
 	std::cout << "    then second player is winner." << std::endl;
 	std::cout << "Else" << std::endl;
 	std::cout << "    first player is winner." << std::endl;
+	
+	// Player first, second;
+	// first.moveIn(game);
+	// do{
+		// second.moveIn(game);
+		// if(game.over()) break;
+		// first.moveIn(game);
+	// }
+	// while(!game.over());
+	// if(game.turn)
+		// std::cout << "Second player is Winner." << std::endl;
+	// else
+		// std::cout << "First player is Winner." << std::endl;
 	return 0;
 } // end main(int argc , char **argv)
 
